253f54713814e383f31243c1b104c9cbb6e3f702
Joekorf <JoernKorfmann@gmx.de>
2022-01-02 18:13:20 +0100

    Set default colors and added the layout for the game activity

--- frontend/android/app/src/main/java/de/oncampus/quizlingo/MainActivity.java
+++ frontend/android/app/src/main/java/de/oncampus/quizlingo/MainActivity.java
@@ -2,13 +2,33 @@ package de.oncampus.quizlingo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Objects.requireNonNull(getSupportActionBar()).hide(); //hides the quizlingo title bar
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewActivity();
+            }
+        });
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
