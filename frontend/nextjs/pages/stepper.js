6a941ee153376392ac4d5a15e043b865ce2b6d26
joernkgrp <j.kogerup@gmail.com>
2022-01-03 02:07:10 +0100

    React useState to change color onClick()

--- frontend/nextjs/pages/stepper.js
+++ frontend/nextjs/pages/stepper.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import {
   Box,
+  Divider,
   MobileStepper,
   Stack,
   Stepper,
   Typography,
   Button,
 } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import { fragen } from "../src/fragen";
 
 export default function TextMobileStepper() {
@@ -14,7 +16,18 @@ export default function TextMobileStepper() {
   const [activeStep, setActiveStep] = React.useState(0);
   const [reveal, setReveal] = useState(false);
   const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [backgroundCol, setbackgroundCol] = useState("gray");
   const maxSteps = fragen.length;
+
+  const MyButton = styled(Button)({
+    backgroundColor: backgroundCol,
+    border: 0,
+    borderRadius: 10,
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    margin: "30px",
+  });
 
   function meinTimeout() {
     setReveal(true);
@@ -30,6 +43,10 @@ export default function TextMobileStepper() {
     const rightAnswer = fragen[activeStep].correctAnswer;
     const answers = document.querySelectorAll(".example");
     answers[rightAnswer].style.backgroundColor = "green";
+
+    if (answers.id == 1) {
+      setbackgroundCol("black");
+    }
     if (index != rightAnswer) {
       answers[index].style.backgroundColor = "red";
     }
@@ -37,6 +54,7 @@ export default function TextMobileStepper() {
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
       setReveal(false);
       answers[index].style.backgroundColor = "";
+
       answers[rightAnswer].style.backgroundColor = "";
     }, 2000);
   }
@@ -44,7 +62,10 @@ export default function TextMobileStepper() {
   return (
     <Box sx={{ maxWidth: 600, flexGrow: 1, p: 2 }}>
       <Typography gutterBottom>{fragen[activeStep].taskText}</Typography>
-      <Typography variant="h3">{fragen[activeStep].questionText}</Typography>
+      <Typography variant="h3" gutterBottom>
+        {fragen[activeStep].questionText}
+      </Typography>
+
       {fragen[activeStep].options.map((option, index) => (
         <p id={index} class="example" onClick={() => answersBackground(index)}>
           {option}
@@ -84,6 +105,15 @@ export default function TextMobileStepper() {
       ) : (
         ""
       )}
+
+      <Divider sx={{ mb: 4 }} />
+
+      <Box sx={{ backgroundColor: backgroundCol, color: "white" }}>
+        Ich bin ein Test
+      </Box>
+      <Box onClick={() => setbackgroundCol("red")}>
+        Klick mich, um die Farbe der Box rot einzufärben.
+      </Box>
 
       <Stepper
         variant="text"
