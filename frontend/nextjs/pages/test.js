7e2c182299a19abe36a2fc2979206f1943526479
joernkgrp <32675242+joernkgrp@users.noreply.github.com>
2022-01-03 14:36:51 +0100

    Add test file to change color and class on click

---
+++ frontend/nextjs/pages/test.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import styles from "../../styles/styles.module.css";
+
+export default function App() {
+  const [myColor, setMyColor] = useState("blue");
+  const [className, setClassName] = useState(styles.menu);
+
+  const divStyle = {
+    color: myColor,
+  };
+
+  function changeColor() {
+    setMyColor("pink");
+  }
+  function changeFarbe() {
+    setClassName(styles.menu2);
+  }
+
+  return (
+    <div>
+      <div style={divStyle} onClick={changeColor}>
+        Hello World! Change color on click
+      </div>
+      <div className={className} onClick={changeFarbe}>
+        Hello World! Change CSS class on click
+      </div>
+    </div>
+  );
+}
